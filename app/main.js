79f7a0937c7153297102917bc28e3af7e87122dc
Bradley Stegbauer <bvstegba@iu.edu>
2019-02-19 09:09:35 -0500

    0.0.1.2 - Laying out the popup

---
+++ app/main.js
@@ -0,0 +1,14 @@
+const portfolio = {
+    popup: document.getElementById("portfolioPopup"),
+    projects: [
+        {
+            name: "",
+            description: "",
+            img: "",
+            link: ""
+        }
+    ],
+    displayPopup(){
+        this.popup.style.display = "block";
+    }
+}
